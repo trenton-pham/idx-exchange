5b96ad874b98b7a0baf8555d8a806a69d830923b
Trenton Pham <trentonp116@gmail.com>
2026-06-28 15:39:45 -0700

    summary stat and visualization + validation update

--- scripts/week2/validation.py
+++ scripts/week2/validation.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import iqr
+from statsmodels.stats.descriptivestats import Description
 import os
 
 RAW_DIR = "../../data/raw/"
@@ -70,6 +73,14 @@ def missing_value_analysis(data):
     missing_df["missing_pct"] = (missing_df["missing_count"] / data.shape[0]) * 100
     return missing_df
 
+def distribution_summary(data):
+    columns = ["ClosePrice", "LivingArea", "DaysOnMarket"]
+    for column in columns:
+        stat_desc = Description(data[column])
+        print(f"Distribution Summary For {column}:\n")
+        print(stat_desc.summary())
+        print("\n")
+
 def main():
     # Process all dates into one csv first
     process_data()
@@ -99,7 +110,7 @@ def main():
     """
 
     # Sold data
-    sold_data = pd.read_csv(PROCESSED_DIR + "CRMLSSold_202401_202605_all.csv")
+    sold_data = pd.read_csv(PROCESSED_DIR + "CRMLSSold_202401_202605_all.csv", low_memory=False)
     missing_sold = missing_value_analysis(sold_data)
 
     missing_sold_filtered = missing_sold[missing_sold["missing_pct"] > 90]
@@ -120,6 +131,118 @@ def main():
     [        'Residential',     'CommercialLease',                'Land',
     'ResidentialLease',  'ManufacturedInPark',   'ResidentialIncome',
       'CommercialSale', 'BusinessOpportunity']
+    """
+
+    distribution_summary(sold_data)
+
+    """
+    Distribution Summary For ClosePrice:
+    Descriptive Statistics  
+    ==========================
+    nobs                639859
+    missing                  7
+    mean             8.802e+05
+    std_err               6814
+    upper_ci         8.936e+05
+    lower_ci         8.669e+05
+    std              5.451e+06
+    iqr                1020000
+    iqr_normal       7.561e+05
+    mad              7.115e+05
+    mad_normal       8.917e+05
+    coef_var             6.193
+    range            989500000
+    max              989500000
+    min                      0
+    skew                 129.7
+    kurtosis         1.866e+04
+    jarque_bera      9.279e+12
+    jarque_bera_pval         0
+    mode                  3500
+    mode_freq         0.006761
+    median              630000
+    1%                    1750
+    5%                    2695
+    10%                   3495
+    25%                  55000
+    50%                 630000
+    75%                1075000
+    90%                1792500
+    95%                2500000
+    99%                4999999
+    --------------------------
+
+    Distribution Summary For LivingArea:
+    Descriptive Statistics     
+    =================================
+    nobs                       639859
+    missing                     44831
+    mean                         3369
+    std_err                      1528
+    upper_ci                     6364
+    lower_ci                      374
+    std                     1.179e+06
+    iqr                           969
+    iqr_normal                  718.3
+    mad                          3279
+    mad_normal                   4109
+    coef_var                    349.9
+    range                   909090909
+    max                     909090909
+    min                             0
+    skew                          771
+    kurtosis                5.946e+05
+    jarque_bera      8765682720079100
+    jarque_bera_pval                0
+    mode                         1440
+    mode_freq                0.006475
+    median                       1580
+    1%                            480
+    5%                            733
+    10%                           896
+    25%                          1184
+    50%                          1580
+    75%                          2153
+    90%                          2924
+    95%                          3540
+    99%                          5467
+    ---------------------------------
+
+    Distribution Summary For DaysOnMarket:
+    Descriptive Statistics  
+    ==========================
+    nobs                639859
+    missing                  0
+    mean                  43.3
+    std_err            0.08732
+    upper_ci             43.47
+    lower_ci             43.13
+    std                  69.85
+    iqr                     45
+    iqr_normal           33.36
+    mad                  38.45
+    mad_normal           48.18
+    coef_var             1.613
+    range                12718
+    max                  12430
+    min                   -288
+    skew                 16.85
+    kurtosis              1749
+    jarque_bera      8.131e+10
+    jarque_bera_pval         0
+    mode                     7
+    mode_freq          0.03976
+    median                  22
+    1%                       0
+    5%                       2
+    10%                      4
+    25%                      9
+    50%                     22
+    75%                     54
+    90%                    104
+    95%                    148
+    99%                    292
+    --------------------------
     """
 
 if __name__ == "__main__":
